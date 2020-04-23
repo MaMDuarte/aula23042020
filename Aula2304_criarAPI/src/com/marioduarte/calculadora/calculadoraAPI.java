416cd4735d8e0b348159ce2800615b0c3baf9d4d
MD <mario.duarte@esg.ipsantarem.pt>
2020-04-23 12:17:33 +0100

    Submissão do Projecto de criação da API

---
+++ Aula2304_criarAPI/src/com/marioduarte/calculadora/calculadoraAPI.java
@@ -0,0 +1,25 @@
+package com.marioduarte.calculadora;
+
+public class calculadoraAPI 
+{
+	public static int soma(int x, int y)
+	{
+		int resultado = x + y;
+		return resultado;
+	}
+	public static int subtracao(int x, int y)
+	{
+		int resultado = x - y;
+		return resultado;
+	}
+	public static int multiplicacao(int x, int y)
+	{
+		int resultado = x * y;
+		return resultado;
+	}
+	public static double divisao(double x, double y)
+	{
+		double resultado = x/y;
+		return resultado;
+	}
+}
